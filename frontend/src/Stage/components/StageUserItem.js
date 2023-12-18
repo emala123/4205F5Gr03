76f144c58f6f02442e40ee947777209a69dfba5d
Christian Lima <lima.christian1@icloud.com>
2023-12-18 10:48:56 -0500

    fin

--- frontend/src/Stage/components/StageUserItem.js
+++ frontend/src/Stage/components/StageUserItem.js
@@ -2,53 +2,75 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import { AuthContext } from '../../shared/context/auth-context';
 import { Link } from "react-router-dom";
+import Modal from 'react-modal';
 
-const StageUserItem = props => {
-    
-    const {error, sendRequest, clearError } = useHttpClient();
-    const auth = useContext(AuthContext);
+const StageUserItem = (props) => {
+  const { error, sendRequest, clearError } = useHttpClient();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const auth = useContext(AuthContext);
 
-    useEffect(() => {
-        if(error){
-          alert(error);
-          clearError();
-        }
-      }, [error, clearError]);
-
-    async function supprimerStage(stageId) {
-        
-        try {
-            const reponseData = await sendRequest(
-              `http://localhost:5000/stages/supprimerStage/${auth.userId}/${stageId}`,
-                "DELETE",
-                JSON.stringify({
-
-                }),
-                {
-                  "Content-Type": "application/json",
-                }
-            );
-            console.log(reponseData);
-            alert("Votre stage à bien été supprimé!");
-        }catch (err) {
-          console.log(err);
-        }
-    
+  const supprimerStageAvecValidation = async () => {
+    try {
+      await supprimerStage(props.id); // Utilisez props.id ici
+      setModalIsOpen(false);
+    } catch (err) {
+      console.log(err);
     }
-    
+  };
 
-    return(
-        <React.Fragment>
-            <li>
-                <div>
-                    <h2>{props.nom}</h2>
-                    <p>{props.description}</p>
-                    <p>{props.courriel}<br/>{props.telephone}<br/>{props.adresse}<br/>{props.remuneration}</p>
-                    <Link to={`/ajouterStage`}> <button onClick={() => supprimerStage(props.id)}>Supprimer ce stage : {props.nom}</button></Link>
-                </div>
-            </li>
-        </React.Fragment>
-    );
+  useEffect(() => {
+    if (error) {
+      // Afficher l'erreur d'une manière plus conviviale dans votre interface utilisateur
+      console.log(error);
+      clearError();
+    }
+  }, [error, clearError]);
+
+  const supprimerStage = async (stageId) => {
+    try {
+      const responseData = await sendRequest(
+        `http://localhost:5000/stages/supprimerStage/${auth.userId}/${stageId}`,
+        "DELETE",
+        JSON.stringify({}),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      console.log(responseData);
+      // Mettez à jour votre interface utilisateur après la suppression
+      alert("Votre stage a bien été supprimé!");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <li>
+        <div>
+          <h2>{props.nom}</h2>
+          <p>{props.description}</p>
+          <p>{props.courriel}<br/>{props.telephone}<br/>{props.adresse}<br/>{props.remuneration}</p>
+          {/* Utilisez une fonction fléchée pour éviter l'appel immédiat de la fonction */}
+          <button onClick={() => setModalIsOpen(true)}>Supprimer ce stage : {props.nom}</button>
+          <Link to={`/ajouterStage`}>
+          </Link>
+        </div>
+      </li>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Confirmer la suppression"
+      >
+        <h2>Confirmer la suppression</h2>
+        <p>Êtes-vous sûr de vouloir supprimer le stage "{props.nom}" ?</p>
+        {/* Utilisez une fonction fléchée pour éviter l'appel immédiat de la fonction */}
+        <Link to={`/ajouterStage`}><button onClick={() => supprimerStageAvecValidation()}>Oui, supprimer</button></Link>­­
+        <button onClick={() => setModalIsOpen(false)}>Annuler</button>
+      </Modal>
+    </React.Fragment>
+  );
 };
 
 export default StageUserItem;
